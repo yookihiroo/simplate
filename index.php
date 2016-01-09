07a7c70706433a5ade8b849eebdc25ffbabc4c6d
yoorwm <yookihiroo@gmail.com>
2016-01-09 00:28:24 +0000

    Part 2.1 - added security

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Web フレームワークをつくろう - Symfony コンポーネントの上に (パート 1)
- * http://docs.symfony.gr.jp/symfony2/create-your-framework/part01.html
+ * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 2)
+ * http://docs.symfony.gr.jp/symfony2/create-your-framework/part02.html
  */
 
-$input = $_GET['name'];
+$input = isset($_GET['name']) ? $_GET['name'] : 'World';
 
-printf('Hello %s', $input);
+header('Content-Type: text/html; charset=utf-8');
+
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
