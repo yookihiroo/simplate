647733a179e4554311e835bf833478da8d1f0c63
yoorwm <yookihiroo@gmail.com>
2016-01-09 04:16:17 +0000

    Part 3.1 - added front.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
 /**
- * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 2)
- * http://docs.symfony.gr.jp/symfony2/create-your-framework/part02.html
+ * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 3)
+ * http://docs.symfony.gr.jp/symfony2/create-your-framework/part03.html
  */
 
-require_once __DIR__.'/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
+require_once __DIR__.'/init.php';
 
 $input = $request->get('name', 'World');
 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
