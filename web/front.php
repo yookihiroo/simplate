ccccb40a477023cfaac786278dd1607148432b74
yoorwm <yookihiroo@gmail.com>
2016-01-09 14:06:22 +0000

    Part 5 - added controller & is_leap_year

--- web/front.php
+++ web/front.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 4)
- * http://docs.symfony.gr.jp/symfony2/create-your-framework/part04.html
+ * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 5)
+ * http://docs.symfony.gr.jp/symfony2/create-your-framework/part05.html
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -9,6 +9,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -18,11 +27,9 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $controller = $request->attributes->get('_controller') ?: 'render_template';
+    $response = call_user_func($controller, $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
