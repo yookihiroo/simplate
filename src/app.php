cfacc424ec39735ed7de78d3f2fd3d2d88d3c946
yoorwm <yookihiroo@gmail.com>
2016-01-10 04:15:22 +0000

    Part 7 - added Simplex framework

--- src/app.php
+++ src/app.php
@@ -6,44 +6,10 @@ use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
-}
-
-class BaseController
-{
-    public function indexAction(Request $request)
-    {
-        return render_template($request);
-    }
-}
-class LeapYearController
-{
-    public function indexAction(Request $request)
-    {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-}
-
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'World',
-    '_controller' => 'BaseController::indexAction',
-)));
-$routes->add('bye', new Routing\Route('/bye', array(
-    '_controller' => 'BaseController::indexAction',
-)));
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => 'LeapYearController::indexAction',
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -1,44 +1,23 @@
 <?php
 /**
- * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 6)
- * http://docs.symfony.gr.jp/symfony2/create-your-framework/part06.html
+ * Web フレームワークをつくろう - Symfony2 コンポーネントの上に (パート 7)
+ * http://docs.symfony.gr.jp/symfony2/create-your-framework/part07.html
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
-
-function render_template($request)
-{
-    extract($request->attributes->all());
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $resolver->getController($request);
-    $arguments = $resolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\ResourceNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Simplex\Framework($matcher, $resolver);
+$response = $framework->handle($request);
 
 $response->send();
